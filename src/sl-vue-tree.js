b265d1a8c5ee23a399a992e7463f5a705c6e9895
shibuya kazuaki <hogemail009@gmail.com>
2021-06-13 19:33:37 +0900

    add selected-with-expanded flag

--- src/sl-vue-tree.js
+++ src/sl-vue-tree.js
@@ -48,6 +48,10 @@ export default {
     maxScrollSpeed: {
       type: Number,
       default: 20
+    },
+    selectedWithExpanded: {
+      type: Boolean,
+      default: false
     }
   },
 
@@ -276,7 +280,7 @@ export default {
           if (shiftSelectionStarted) nodeModel.isSelected = node.isSelectable;
         } else if (node.pathStr === selectedNode.pathStr) {
           nodeModel.isSelected = node.isSelectable;
-        } else if (!node.isExpanded && selectedNode.pathStr.indexOf(node.pathStr.slice(0, -1)) > -1) {
+        } else if (this.selectedWithExpanded && (!node.isExpanded && selectedNode.pathStr.indexOf(node.pathStr.slice(0, -1)) > -1)) {
           nodeModel.isExpanded = true;
           nodeModel.isSelected = false;
         } else if (!addToSelection) {
